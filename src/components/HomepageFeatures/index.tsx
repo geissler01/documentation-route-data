f3250531ffa73da7e804f5c16732ca85eb31835c
jennlopezdv <jenniferlopezdavid71@gmail.com>
2026-04-09 19:48:08 -0500

    prueba Jenn

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -31,7 +31,7 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: 'Imagenessss',
+    title: 'Este es el mejor comentario del mundo. Att: Jenn lo',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
